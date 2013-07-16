ddea9bf2b4e096a6f4fadb4a60c4f9ba8403921b
Austin King <shout@ozten.com>
2013-07-16 11:15:50 -0700

    Fixed scope / mutability issue across callback

--- hello.rs
+++ hello.rs
@@ -14,26 +14,37 @@ use std::vec;
 use http_client::uv_http_request;
 use http_client::RequestEvent;
 
+pub struct RepoResponse {
+    rawJson: ~[~str]
+}
+
+impl RepoResponse {
+    fn add(&mut self, data: ~str) {
+        self.rawJson.push(data)
+    }
+}
 
 fn main() {
 
     // http://ozten.com/psto/
     // https://api.github.com/repositories
-    let u: Url = url::from_str("https://api.github.com/repositories").get();
+    let u: Url = url::from_str("http://ozten.com/random/sample.json").get();
     let mut request = uv_http_request(u);
 
+    let res = @mut RepoResponse{rawJson: ~[]};
+
     do request.begin |event| {
-        let mut r:~[~str] = vec::with_capacity(1000000);
-        //r.push(~"foo");
-        //r.push(~"bar");
         match event {
             http_client::Error(e) => {
                 println(fmt!("Ouch... error %?", e));
             },
             http_client::Status(s) => {
-                println(fmt!("Status %?", s));
-                println(r.concat());
-                match json::from_str(r.concat()) {
+                //println(fmt!("Status %?", s));
+
+                //res.rawJson.push(fmt!(" status event [%?] ", s));
+                println(fmt!("Final payload was \n=========\n%?\n===========", res.rawJson.concat()));
+
+                match json::from_str(res.rawJson.concat()) {
                     Ok(json) => {
                         match json {
                             json::Object(o) => {
@@ -55,29 +66,18 @@ fn main() {
             },
             http_client::Payload(p) => {
                 let data = p.take();
-                println(fmt!("Concating %?", str::from_bytes(data)));
-                r.push(str::from_bytes(data));
-                //r = r + str::from_bytes(data);
-                //r.push_str(str::from_bytes(data));
-                //r = r + str::from_bytes(data);
-                println(fmt!("%?", r.len()));
-                //println(fmt!("%?\n\n", r.concat()));
-
-                // I think we need to know how many bytes and then
-                // do the JSON parsing here...
+                res.rawJson.push(str::from_bytes(data));
             }
         }
-
-
     }
-    println("Hardcoded json play");
+
     // Problem #2 How to output parts of JSON after parsing
     // I think this is working...
     match json::from_str("{\"ham\": \"bone\"}") {
         Ok(obj) => {
             match obj {
                 json::Object(o) => {
-                    println(fmt!("Payload! %?", o.get(& ~"ham")))
+                    //println(fmt!("Payload! %?", o.get(& ~"ham")))
                 }
                 _ => { println("Skipping") }
             }
@@ -97,5 +97,5 @@ fn main() {
     let mut a:~[~str] = ~[];
     a.push(~"Hello");
     a.push(~" World");
-    println(a.concat());
+    //println(a.concat());
 }
