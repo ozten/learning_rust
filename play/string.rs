3b558e66507fbdcc6d3c23fbf45fedb09ec570ff
Austin King <shout@ozten.com>
2013-07-23 17:46:57 -0700

    Adding string play

---
+++ play/string.rs
@@ -0,0 +1,19 @@
+// rustc string.rs && RUST_LOG=string=4 ./string
+
+fn main() {
+    let s = "<foo bar>";
+    match s.find('<') {
+        Some(start) => {
+            println(fmt!("%?", start));
+            match s.find('>') {
+                Some(end) => {
+                    println(s.slice(start + 1, end));
+                },
+                _ => { fail!("Missing >"); }
+            }
+
+        },
+        _ => {fail!("Missing <"); }
+    }
+    println(s);
+}
