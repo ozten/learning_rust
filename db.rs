890e9d71a98e0d47fb17781a64b00539289ba6cd
Austin King <shout@ozten.com>
2013-07-23 20:09:35 -0700

    Create DB table on startup. Select next_url

--- db.rs
+++ db.rs
@@ -1,6 +1,8 @@
 extern mod sqlite;
 
-use sqlite::*;
+use sqlite::open;
+use sqlite::database::Database;
+use sqlite::types::*;
 
 static create_repo_meta:&'static str =
     "CREATE TABLE IF NOT EXISTS repo_meta (   \
@@ -20,45 +22,58 @@ static next_repo_url:&'static str =
      WHERE next_url IS NULL AND json IS NULL  \
      ORDER BY created DESC LIMIT 1;";
 
-
-fn setup() {
-    debug!("Database starting");
-    match open("github.sqlite") {
-        Ok(database) => {
-            // TODO Check ResultCode enum
-            let res:types::SqliteResult<bool> =
-                database.exec(create_repo_meta);
-            match res {
-                Ok(true) => { debug!("Ensured repo_meta exists"); },
-                Ok(false) => { debug!("Failed creating repo_meta"); },
-                Err(e) => { fail!(fmt!("%?", e)); }
-            }
-            /*
-            println(fmt!("CREATE %?", res));
-            println(fmt!("INSERT %?", database.exec("INSERT INTO test (foo) VALUES (\"hamster dance\");")));
-            let cur = database.prepare("SELECT * FROM test", &None);
-            match cur {
-                Ok(ref stmt) => {
-                    debug!(stmt);
-                    stmt.step();
-                    debug!(stmt.get_text(0));
-                },
-                Err(ref e) => {println(fmt!("%?", e));}
-            }
-            println(fmt!("SELECT %?", cur));
-            */
-        },
-        Err(e) => {
-            fail!("Unable to open sqlite database")
-        }
+fn setup(database: &Database) {
+    let res:SqliteResult<bool> = database.exec(create_repo_meta);
+    match res {
+        Ok(true) => { debug!("Ensured repo_meta exists"); },
+        Ok(false) => { debug!("Failed creating repo_meta"); },
+        Err(e) => { fail!(fmt!("%?", e)); }
     }
 }
 
+fn next_url(database: &Database) -> ~str {
+        let cur = database.prepare(next_repo_url, &None);
+        match cur {
+            Ok(ref stmt) => {
+                if (stmt.step() == SQLITE_DONE) {
+                    return ~"https://localhost:8002/repositories";
+                } else {
+                    return stmt.get_text(0);
+                }
+            },
+            Err(ref e) => {
+                fail!("%?", e);
+            }
+        };
+}
+
+/*
+database.prepare
+let sth = checked_prepare(database, "SELECT id FROM test WHERE id > ? AND id < ?");
+    assert!(sth.bind_param(1, &Integer(2)) == SQLITE_OK);
+    assert!(sth.bind_param(2, &Integer(4)) == SQLITE_OK);
+
+    assert!(sth.step() == SQLITE_ROW);
+    assert!(sth.get_num(0) as int == 3);
+    */
+fn insert_db() {
+    //println(fmt!("INSERT %?", database.exec("INSERT INTO test (foo) VALUES (\"hamster dance\");")));
+}
+
 // Task
-fn init_db(start_channel:&Chan<~str>) {
-    println("Calling setup");
-    setup();
-    start_channel.send(~"https://localhost:8002/repositories");
+fn init_db(start_channel:&Chan<~str>) -> () {
+    debug!("Database starting");
+    let database = match open("github.sqlite") {
+        Ok(db) => db,
+        Err(e) => {
+            fail!("Unable to open sqlite database")
+        }
+    };
+    setup(&database);
+    start_channel.send(
+
+        next_url(
+            &database));
     //loop {
         // wait for read or writes to the database
         // execute them
